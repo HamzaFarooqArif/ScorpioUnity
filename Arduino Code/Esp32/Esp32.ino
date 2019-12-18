e0f0f79a4809006ae079f83ec8ab4a8e99742c8f
Hamza <m.hamza.arif786@gmail.com>
2019-12-18 22:13:59 +0500

    URL Params

--- Arduino Code/Esp32/Esp32.ino
+++ Arduino Code/Esp32/Esp32.ino
@@ -1,99 +1,51 @@
-#include <WebServer.h>
-#include "index_html.h"
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+ 
+const char* ssid = "ZONG MBB-E5573-AE26";
+const char* password =  "58688303";
 
-#define LEDn    12
-//#define LEDpwm   2
+char out_str[40];  
 
-#define LEDC_CHANNEL_0     0
-#define LEDC_TIMER_13_BIT  13
-#define LEDC_BASE_FREQ     5000
-#define LED_PIN            2
+AsyncWebServer server(80);
 
-
-//Define name of the Wifi & password for creating an access point
-const char* ssid = "esp32";           //"ZONG MBB-E5573-AE26";
-//!!!Your password MUST be a minimum of 8 characters...otherwise neither password nor ssid will be accepted -> default or old ssid&pwd will show up!!! 
-const char* password = "12345678";    //"58688303";
-
-
-WebServer server(80);
-
-bool led;
-int pwm;
-int pwmCount;
-
-void ledON(){
-  led=1;
-}
-
-void ledOFF(){
-  led=0;  
-}
-//pwm functions - if pwm is set to 1/-1 it will in-/decrease pwmCounter in loop()
-void plus(){
-  pwm=3;  
-}
-
-void minus(){
-  pwm=-3;  
-}
-
-void stopPWM(){
-  pwm=0;  
-}
-//----------------------------------------------------------------------------------
-void handleRoot(){
-  server.send(200, "text/html", webpage);  
-}
-
-void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) 
-{
-  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
-  ledcWrite(channel, duty);
-}
-
-void setup() {
-  //Setup Analog________________
-  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
-  ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
-  //Setup Serial________________
-  Serial.begin(9600);
-  //Set LED pins to output________________
-  pinMode(LEDn,OUTPUT);
-  //pinMode(LEDpwm,OUTPUT);
-  //Wifi as access point__________________
-  WiFi.mode(WIFI_AP);
-  IPAddress apLocalIp(2,2,2,2);
-  IPAddress apSubnetMask(255,255,255,0);
-  WiFi.softAPConfig(apLocalIp,apLocalIp,apSubnetMask);
-  WiFi.softAP(ssid, password);
-  //Server________________________________
-  server.begin();
-  server.on("/", handleRoot);
-  server.on("/LEDon", ledON);     
-  server.on("/LEDoff", ledOFF);   
-  server.on("/minus", minus);   
-  server.on("/plus", plus);   
-  server.on("/stop", stopPWM);   
-  //initialize variables__________________
-  pwm=0;
-  pwmCount=0;
-  led=0;
-}
-
-void loop() {
-  server.handleClient();
-  //turns LED ON/OFF (GPIO 2/D4)
-  digitalWrite(LEDn, led);
+int paramArray[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+ 
+void setup(){
+  Serial.begin(115200);
   
-  //in-/decreases pwmCount(=brightness)
-  pwmCount+=pwm;
-  //to hold pwmCount in it's range (min 0 - max 1024)
-  if(pwmCount>1024)pwmCount=1024;
-  if(pwmCount<0)pwmCount=0;
-  //Serial.println(pwmCount);
-  //sets pwmCount to PWM value of GPIO 12/D6
-  //analogWrite(LEDpwm, pwmCount);
-  ledcAnalogWrite(LEDC_CHANNEL_0, pwmCount);
-  delay(10);
+  WiFi.begin(ssid, password);
+  
+  strcpy(out_str, "Connecting to ");
+  strcat(out_str, ssid);
+    
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(out_str);
+  }
+ 
+  Serial.println(WiFi.localIP());
+ 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+ 
+    int paramsNr = request->params();
+    Serial.println(paramsNr);
+ 
+    for(int i=0;i<paramsNr;i++){
+ 
+        AsyncWebParameter* p = request->getParam(i);
+        paramArray[i] = p->value().toInt();
+        Serial.print("Param index: ");
+        //Serial.println(p->name());
+        Serial.println(i);
+        Serial.print("Param value: ");
+        Serial.println(paramArray[i]);
+        Serial.println("------");
+    }
+ 
+    request->send(200, "text/plain", "message received");
+  });
+ 
+  server.begin();
 }
+ 
+void loop(){}
