ae86597c03c2f57cca69981606670b87bb44fd1b
Hamza <m.hamza.arif786@gmail.com>
2019-12-25 13:25:11 +0500

    URL Individual Analog Control

--- Arduino Code/Esp32/Esp32.ino
+++ Arduino Code/Esp32/Esp32.ino
@@ -1,5 +1,8 @@
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
+
+#define LEDC_TIMER_13_BIT  13
+#define LEDC_BASE_FREQ     5000
  
 const char* ssid = "ZONG MBB-E5573-AE26";
 const char* password =  "58688303";
@@ -8,16 +11,58 @@ char out_str[40];
 
 AsyncWebServer server(80);
 
-int paramArray[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
- 
+const int chCount = 9;
+int paramArray[chCount] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int pinsArray[chCount] = {15, 2, 4, 16, 17, 5, 18, 19, 21};
+
+void setupAnalogWrite()
+{
+  for(int i = 0; i < chCount; i++)
+  {
+    ledcSetup(i, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+    ledcAttachPin(pinsArray[i], i);
+  }
+}
+
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
+  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
+  ledcWrite(channel, duty);
+}
+
+
+void printArray()
+{
+  for(int i = 0; i < chCount; i++)
+    {
+      String result = String(paramArray[i]);
+      Serial.print(result + " ");
+    }
+    Serial.println();
+}
+
+void writeValue()
+{
+  for(int i = 0; i < chCount; i++)
+  {
+    ledcAnalogWrite(i, paramArray[i]);
+  }
+}
+
 void setup(){
+  setupAnalogWrite();
+  
   Serial.begin(115200);
+
+  for(int i = 0; i < 9; i++)
+  {
+    pinMode(pinsArray[i], OUTPUT);
+  }
   
   WiFi.begin(ssid, password);
   
   strcpy(out_str, "Connecting to ");
   strcat(out_str, ssid);
-    
+   
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println(out_str);
@@ -28,19 +73,27 @@ void setup(){
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
  
     int paramsNr = request->params();
-    Serial.println(paramsNr);
+    //Serial.println(paramsNr);
  
     for(int i=0;i<paramsNr;i++){
  
         AsyncWebParameter* p = request->getParam(i);
-        paramArray[i] = p->value().toInt();
-        Serial.print("Param index: ");
+        if(p->name().toInt() < chCount)
+        {
+          paramArray[p->name().toInt()] = p->value().toInt();
+        }
+        
+        //paramArray[i] = p->value().toInt();
+        //Serial.print("Param index: ");
         //Serial.println(p->name());
-        Serial.println(i);
-        Serial.print("Param value: ");
-        Serial.println(paramArray[i]);
-        Serial.println("------");
+        //Serial.println(i);
+        //Serial.print("Param value: ");
+        //Serial.println(paramArray[i]);
+        //Serial.println("------");
     }
+    
+    writeValue();
+    printArray();
  
     request->send(200, "text/plain", "message received");
   });
